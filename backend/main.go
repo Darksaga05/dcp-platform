025e3debd819555b373665ac4706019846f60b3e
Darksaga05 <alakasmurat5@gmail.com>
2026-07-10 09:27:56 +0300

    Connect message API route
    Added the messages API route to connect the message handler with the backend server.

--- backend/main.go
+++ backend/main.go
@@ -14,7 +14,8 @@ func main() {
 
 	http.HandleFunc("/health", handlers.HealthCheck)
 	http.HandleFunc("/user", handlers.GetUser)
-
+	http.HandleFunc("/messages", handlers.GetMessages)
+	
 	fmt.Println(appConfig.AppName + " server started on port " + appConfig.ServerPort)
 
 	err := http.ListenAndServe(":"+appConfig.ServerPort, nil)
